a123d7cbf8b2fd0d44b69b4320476f189f29338f
Oleg Gruntov <suppergruntov@gmil.com>
2022-06-25 02:03:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  if (n > 0){
+   let rev = n.toString().split('').reverse().join('');
+    return rev * 1;
+  }else if( n < 0){
+    n = n * -1
+   let re = n.toString().split('').reverse().join('');
+    return re;
+  }else{
+    return 0
+  }
 }
